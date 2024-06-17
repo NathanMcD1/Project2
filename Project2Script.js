d98368f2ef081d4b7712fadf873e64cdc4b18f92
NathanMcD1 <131803912+NathanMcD1@users.noreply.github.com>
2024-06-17 14:45:58 -0400

    a

--- Project2Script.js
+++ Project2Script.js
@@ -1,7 +1,7 @@
 function calculate(){
-    let n = document.getElementById("yearInput").value*12;
-    let p = document.getElementById("principalInput").value;
-    let r = document.getElementById("yearlyRateInput").value/1200;
-    let payment = (p*r)/(1-Math.pow((1 + r), -n));
-    document.getElementById("paymentOutput").innerHTML="$"+payment.toFixed(2);
+    let n = parseFloat(document.getElementById("yearInput").value) * 12;
+    let p = parseFloat(document.getElementById("principalInput").value);
+    let r = parseFloat(document.getElementById("yearlyRateInput").value) / 1200;
+    let payment = (p * r) / (1 - Math.pow((1 + r), -n));
+    document.getElementById("paymentOutput").innerHTML = "$" + payment.toFixed(2);
 }
